66347671aa33a52fcb6c0e483ac975ff25ef099e
Andy Liu <aliu927@gmail.com>
2020-11-14 23:19:22 -0800

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+const addButtonElem = document.querySelector('.addBook');
+addButtonElem.addEventListener('click',(e) => {
+    //console.log(e.target.classList.value);
+    let addBookHide = e.target;
+    if(addBookHide === addButtonElem){
+        //console.log(addBookHide);
+        const containerHide = addBookHide;
+        console.log(containerHide);
+    }
+});
+
 let myLibrary = [];
 
 function Book(){
